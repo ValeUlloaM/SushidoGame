6e62d7e816ce329bb04f25dc7d62ef3b92505409
Manuela <manuella23bermudez@gmail.com>
2021-06-09 22:07:11 -0500

    clases de acertijos

---
+++ Pista.js
@@ -0,0 +1,13 @@
+class Pista{
+
+    constructor(pistaImagen){
+        this.pistaImagen = pistaImagen;
+    }
+
+   pintar(){
+       imageMode(CENTER);
+       image(this.pistaImagen, 600, 350);
+   }
+
+
+}
